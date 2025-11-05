8a2156395431bdadf308d694323228d0fa2c7ca2
alexNovaa <alexnovak.media@gmail.com>
2025-11-05 06:35:32 -0800

    Create App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>VeeSafe is Live 🚀</h1>
+      <p>Your app successfully deployed on Render!</p>
+    </div>
+  );
+}
